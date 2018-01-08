f13937f2a3a2196405d6e6a79585c47bf7a2356d
Asa Baylus <asa@baylus.com>
2018-01-07 22:40:35 -0500

    add  ponies

--- src/app.jsx
+++ src/app.jsx
@@ -2,15 +2,20 @@ import React from 'react';
 import Navigation from './components/Navigation';
 import 'normalize.css';
 import 'styles/index.scss';
+import Pony from 'react-pony';
 
 const App = () => (
   <div className='App'>
+    <h1>ES6</h1>
+    <Pony apiKey="xe8c740Lt4F7dDcsFCIw6HkpJzxmCFKJ" />
+    {/*
     <Navigation/>
     <div>
       <h1>It Works!</h1>
       <p>This React project just works including <span className="redBg">module</span> local styles.</p>
       <p>Enjoy!</p>
     </div>
+    */}
   </div>
 );
 
